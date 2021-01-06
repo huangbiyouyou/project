fed2a156273ab939b627679a57f3be4a34864355
黄壁友 <947757582@qq.com>
2021-01-06 17:16:52 +0800

    youyou2测试提交

---
+++ src/main/java/com/project/demo/controller/YouyouOneController.java
@@ -0,0 +1,8 @@
+package com.project.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class YouyouOneController extends BaseController{
+
+}
