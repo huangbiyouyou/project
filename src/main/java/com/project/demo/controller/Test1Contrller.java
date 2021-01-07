39ebda2292687f09bcd0c0e35a1602d68ab014f1
黄壁友 <947757582@qq.com>
2021-01-07 15:28:45 +0800

    test1测试提交

---
+++ src/main/java/com/project/demo/controller/Test1Contrller.java
@@ -0,0 +1,7 @@
+package com.project.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Test1Contrller extends BaseController{
+}
---
+++ src/main/java/com/project/demo/controller/Test2Contrller.java
@@ -0,0 +1,7 @@
+package com.project.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Test2Contrller extends BaseController{
+}
